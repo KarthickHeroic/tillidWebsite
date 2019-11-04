957cb9f9a87371214af2cf67ceb10ebcbb120123
KarthickHeroic <karthick.veeramani@gmail.com>
2019-11-04 19:56:52 +0530

    First Commit
    First Commit

---
+++ config.rb
@@ -0,0 +1,24 @@
+require 'compass/import-once/activate'
+#require 'sass-globbing'
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "scss"
+images_dir = "images"
+javascripts_dir = "javascripts"
+
+output_style = :nested
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+line_comments = false
+relative_assets = true
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass scss scss && rm -rf sass && mv scss sass
